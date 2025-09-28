c1ea792ef9c3200f418ba97b0f2cf17eb5aa7ca3
zhenglixin <lixin.zhenglx@gmail.com>
2025-09-29 00:15:02 +0800

    integrate the exporter into the gnb process.

--- src/gnb_core.c
+++ src/gnb_core.c
@@ -920,6 +920,10 @@ void gnb_core_stop(gnb_core_t *gnb_core){
         gnb_core->node_worker->stop(gnb_core->node_worker);
     }
 
+    if (gnb_core->exporter_worker) {
+        gnb_core->exporter_worker->stop(gnb_core->exporter_worker);
+    }
+
     gnb_core->loop_flag = 0;
 
 #ifdef __UNIX_LIKE_OS__
